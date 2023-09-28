61bda3b26ffdf54827cfa989445e336b0d393a09
DESKTOP-K3L9297\David <david.pozhar@gmail.com>
2023-09-28 23:49:50 +0300

    added 3rd lab

---
+++ lab3/js/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".home__slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    loop: true,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
